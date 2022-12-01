3ca53bd9210cc55725a2dedf2a4cda61a6cd4416
PHILIP <philophilo037@gmail.com>
2022-12-01 19:43:59 +0300

    completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
     def show
-        render json:User.all
+      user = User.find_by(id: session[:user_id])
+      if user
+        render json: user
+      else
+        render json: { error: "Not authorized" }, status: :unauthorized
+      end
     end
 end
